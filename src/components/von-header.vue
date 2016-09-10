4ee905b1603cdd00f46f80be8e2326507ae3432c
wangdahoo <157195705@qq.com>
2016-09-10 16:35:32 +0800

    init

---
+++ src/components/von-header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="{{ headerClass }}">
+    <!-- backButton -->
+    <button v-show="showBackButton" class="button button-clear">
+        <i class="icon ion-ios-arrow-back positive"></i> <span class="positive"> {{ backButton.text }}</span>
+    </button>
+
+    <h1 class="title">{{ title }}</h1>
+  </div>
+</template>
+<style lang="sass">
+
+</style>
+<script>
+  export default{
+    props: {
+      title: String,
+      headerClass: {
+        String,
+        default() {
+          return 'bar bar-header bar-light'
+        }
+      },
+      showBackButton: {
+        type: Boolean,
+        default() {
+          return true
+        }
+      },
+      backButton: {
+        type: Object,
+        default() {
+          return {
+            text: 'Back'
+          }
+        }
+      }
+    },
+
+    methods: {
+
+    }
+  }
+</script>
