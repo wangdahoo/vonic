900408fc53c319fe42119e58b7272f50a8ebc127
wangdahoo <157195705@qq.com>
2016-11-07 15:37:41 +0800

    v0.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -64,11 +64,35 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'doc') {
 
   // docs
   module.exports.output.path = path.resolve(__dirname, './docs')
   module.exports.output.publicPath = ''
+
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ])
+}
+
+if (process.env.NODE_ENV === 'production') {
+
+  // distribution
+  module.exports.entry = './src/index.js'
+  module.exports.output.publicPath = ''
+  module.exports.output.filename = 'vonic.js'
 
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
