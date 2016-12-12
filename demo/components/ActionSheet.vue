8d36acbe89016d1f2d8f330d68f75b1e1a123dc7
wangdahoo <157195705@qq.com>
2016-12-12 21:31:09 +0800

    updat edemo

---
+++ demo/components/ActionSheet.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="page has-navbar" v-nav="{title: 'ActionSheet', showBackButton: true, onBackButtonClick: back}">
+    <div class="page-content padding-top">
+      <div class="item item-icon-right" @click="showActionSheet()">
+        iOS<span class="note">默认</span>
+        <i class="icon ion-ios-arrow-right"></i>
+      </div>
+
+      <div class="item item-icon-right" @click="showActionSheet('android')">
+        Android<span class="note"></span>
+        <i class="icon ion-ios-arrow-right"></i>
+      </div>
+
+      <div class="item item-icon-right" @click="showActionSheet('weixin')">
+        微信<span class="note"></span>
+        <i class="icon ion-ios-arrow-right"></i>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+
+  .item-divider:first-of-type {
+    border-top: none;
+  }
+
+  .item-icon-right {
+    .icon {
+      right: 0;
+      color: #ccc;
+      font-size: 24px;
+    }
+
+    span.note {
+      color: #aaa;
+      font-size: 13px;
+      float: right;
+      margin-right: 15px;
+    }
+  }
+
+</style>
+<script>
+  export default {
+    methods: {
+      showActionSheet(theme) {
+        $actionSheet.show({
+          theme: theme || '',
+          title: '标题',
+          buttons: {
+            'Action - 1': () => {
+              console.log('action 1 called.')
+            },
+
+            'Action - 2': () => {
+              console.log('action 2 called.')
+            }
+          }
+        })
+      },
+
+      back() {
+        $router.back({path: '/'})
+      }
+    }
+  }
+</script>
