57ebd4c6f9d10581b559df5838f701e5248e7f42
wangdahoo <157195705@qq.com>
2017-02-25 21:51:48 +0800

    remove useless code

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -1,103 +1,40 @@
 <template>
-  <div von-nav v-if="!hideNavbar" class="navbar">
-    <div class="back-button"
-      v-if="showBackButton"
-      v-html="backButtonText"
-      @click="backButtonClicked($event)"
-    ></div>
+  <div von-nav class="navbar">
+    <div class="navbar-container">
 
-    <div class="center">
-      <span class="title" v-text="title"></span>
     </div>
-
-    <div class="menu-button"
-      v-if="showMenuButton"
-      v-html="menuButtonText"
-      @click="menuButtonClicked($event)"
-    ></div>
   </div>
 </template>
 <style lang='scss'>
-  @import "../scss/variables";
-  @import '../scss/mixins';
-
   $navbar-z-index: 10;
-  $navbar-title-z-index: 12;
-  $navbar-button-z-index: 13;
-
-  // .navbar[visible] {
-  //   visibility: visible;
-  // }
 
   .navbar {
-    // visibility: hidden;
+    position: fixed;
+    z-index: $navbar-z-index;
 
-    // position: fixed;
-    position: absolute;
+    .navbar-container {
+      position: absolute;
+    }
+  }
+
+  .navbar,
+  .navbar > .navbar-container {
     left: 0;
     top: 0;
     width: 100%;
     height: 44px;
-    background-color: #fff;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-    -webkit-tap-highlight-color: rgba(0,0,0,0);
-    z-index: $navbar-z-index;
-
-    .back-button, .menu-button {
-      position: absolute;
-      top: 0;
-      width: 80px;
-      height: 44px;
-      line-height: 44px;
-      z-index: $navbar-button-z-index;
-
-      .button-icon {
-        padding: 0;
-        min-height: 44px;
-        height: 44px;
-        margin-top: -1px;
-
-        &.ion-android-arrow-back {
-          &:before {
-            font-size: 28px;
-          }
-        }
-      }
-    }
-
-    .back-button {
-      left: 0;
-      padding: 0 0 0 10px;
-      text-align: left;
-    }
-
-    .menu-button {
-      right: 0;
-      padding: 0 10px 0 0;
-      text-align: right;
-    }
-
-    .center {
-      position: absolute;
-      top: 0px;
-      left: 0;
-      width: 100%;
-      height: 44px;
-      padding: 0;
-      text-align: center;
-      z-index: $navbar-title-z-index;
-
-      .title {
-        font-size: 18px;
-        line-height: 44px;
-        // opacity: 0;
-      }
-    }
   }
 </style>
 <script>
   import assign from 'object-assign'
   import channel from './channel'
+  import Vue from 'vue'
+
+  const timeout = (duration = 0) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, duration);
+    })
+  }
 
   const is_ios_device = () => {
     return /iPad|iPhone|iPod/.test(navigator.userAgent)
@@ -114,14 +51,6 @@
   }
 
   window.__block_touch__ = false
-
-  function navTransitionStart() {
-    window.__block_touch__ = true
-  }
-
-  function navTransitionEnd() {
-    window.__block_touch__ = false
-  }
 
   const NAV_DEFAULT = {
     title: '',
@@ -152,6 +81,8 @@
       let el = this.$el
       channel.$on('PageTransitionEvent', (data) => {
         assign(this.$data, NAV_DEFAULT, data)
+
+        console.log(this.$data)
       })
     },
 
@@ -179,13 +110,6 @@
 
         if (this.onMenuButtonClick) {
           this.onMenuButtonClick()
-        }
-      },
-
-      animateTitle() {
-        let title = this.$el.querySelector('.center .title')
-        if (title) {
-
         }
       }
     }
