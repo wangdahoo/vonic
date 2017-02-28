bf944b96d0fcb1c7892f335dceb0a23b61637610
wangdahoo <157195705@qq.com>
2017-02-28 21:38:49 +0800

    v2.0.0-alpha.27

--- demo/components/Test.vue
+++ demo/components/Test.vue
@@ -3,9 +3,22 @@
     <div class="page-content padding">
       <button class="button button-assertive button-block"
         @click="showLoading()">Update Loading Text</button>
+
+        <h1>字体规范 ¥1.50 1234567890</h1>
+        <h2>字体规范 ¥1.50 1234567890</h2>
+        <h3>字体规范 ¥1.50 1234567890</h3>
+        <h4>字体规范 ¥1.50 1234567890</h4>
+        <h5>字体规范 ¥1.50 1234567890</h5>
+        <h6>字体规范 ¥1.50 1234567890</h6>
+
     </div>
   </div>
 </template>
+<style lang="scss">
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 300;
+  }
+</style>
 <script>
   export default {
     methods: {
