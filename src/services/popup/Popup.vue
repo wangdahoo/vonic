a157691631c6af134be2b76012f043f303c9df7f
wangdahoo <157195705@qq.com>
2017-03-04 19:27:42 +0800

    v2.0.0-beta.4

--- src/services/popup/Popup.vue
+++ src/services/popup/Popup.vue
@@ -30,7 +30,7 @@
           @click="hide(index)"
         >
         <div class="hairline-top"></div>
-        <div class="hairline-right"></div>
+        <div class="hairline-right" v-if="index < buttons.length - 1"></div>
         <span v-text="b.text"></span>
         </button>
       </div>
